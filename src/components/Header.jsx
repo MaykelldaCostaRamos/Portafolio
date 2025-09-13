52271b9dc9335dc73a6737976b4fcba8e378773e
TheBos-s <mkpako2009@gmail.com>
2025-09-14 01:53:26 +0200

    Add logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 export default function Header() {
   return (
     <header>
-      <div className="flex justify-center items-center gap-10 bg-slate-100 rounded-full p-2">
-          <h1>M<strong> | </strong>R</h1>
+      <div className="flex justify-center items-center gap-8 bg-slate-100 rounded-full p-2">
+          
+            <h1 className="bg-slate-500/10 font-extrabold rounded-full p-2">M<strong>|</strong>R</h1>
+          
           <a href="">Sobre mí</a>
           <a href="">Proyectos</a>
           <a href="">Contacto</a>
