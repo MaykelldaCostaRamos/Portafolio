2b55c4b2e7a2cad305f921e507369eedaac2efc8
TheBos-s <mkpako2009@gmail.com>
2025-09-14 01:43:11 +0200

    The first commit: upload initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="flex justify-center items-center gap-10 bg-slate-100 rounded-full p-2">
+          <h1>M<strong> | </strong>R</h1>
+          <a href="">Sobre mí</a>
+          <a href="">Proyectos</a>
+          <a href="">Contacto</a>
+        
+      </div>
+
+
+
+    </header>
+  );
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <>
+      <div className="flex justify-center items-center gap-2 py-9">
+        <img
+          className="size-36 rounded-full"
+          src="/maykellramos.webp"
+          alt="Maykell Ramos"
+        />
+        <h1 className="text-3xl font-bold">Maykell Ramos</h1>
+      </div>
+        <p className="text-lg mt-2">
+          Full-Stack Junior | Apasionado por JavaScript, React y la creación de
+          experiencias web eficientes y atractivas.
+        </p>
+    </>
+  );
+}
