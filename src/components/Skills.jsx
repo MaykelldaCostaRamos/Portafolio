640806b72ee65d0167a1efd53d98d746f446b410
TheBos-s <mkpako2009@gmail.com>
2025-09-15 10:18:34 +0200

    Add svg

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,20 +1,67 @@
+import { motion } from "framer-motion";
+
+import { ReactComponent as HTMLIcon } from "../assets/svg/html5.svg";
+import { ReactComponent as CSSIcon } from "../assets/svg/css3.svg";
+import { ReactComponent as JSIcon } from "../assets/svg/js.svg";
+import { ReactComponent as NodeIcon } from "../assets/svg/node-js.svg";
+import { ReactComponent as REACTIcon } from "../assets/svg/react.svg";
+
+export default function Skills() {
+  const skills = ["HTML", "CSS3", "JavaScript", "React-JS", "NODE.JS"];
+  const loopSkills = [...skills, ...skills];
 
 
-export default function Skills () {
+  const skillIcons = {
+    HTML: HTMLIcon,
+    CSS3: CSSIcon,
+    JavaScript: JSIcon,
+    "React-JS": REACTIcon,
+    "NODE.JS": NodeIcon 
+  }
 
-    const skills = ['HTML', 'CSS3', 'JavaScript', 'React-JS', 'PHP', 'NODE.JS'];
 
-    return (
 
-        <section id="skills">
-            <div className="flex w-full pt-6">
-                <p className="flex-col bg-slate-100 text-lg font-bold p-2">Work for This</p>
-                {skills.map((skill, i) => 
-                <span key={i} className="flex-1 py-8 text-white bg-black text-center">{skill}</span>
-                )}
-            </div>
-        </section>
+  return (
+    <section id="skills" className="w-full overflow-hidden">
+      <div className="px-6">
+        <div className="relative flex items-center w-full mt-4 overflow-hidden">
+          <p className="bg-indigo-600 shadow-sm shadow-indigo-400 text-slate-100 text-lg font-bold p-2 z-10 shrink-0 rounded-lg">
+            Stack
+          </p>
 
-    );
+          
+          <motion.div
+            className="absolute left-0 flex gap-8 whitespace-nowrap will-change-transform"
+            animate={{ x: ["0%", "-50%"] }}
+            transition={{
+              repeat: Infinity,
+              duration: 15,
+              ease: "linear",
+            }}
+          >
+         {/*    {loopSkills.map((skill, i) => (
+              <span
+                key={i}
+                className="text-black text-lg font-semibold px-4"
+              >
+                {skill}
+              </span>
+            ))}*/}
 
+
+            {loopSkills.map((skill, i) => {
+                const Icon = skillIcons[skill]; // obtenemos el SVG correspondiente
+                return (
+                    <span key={i} className="flex items-center gap-2 text-black text-lg font-semibold px-4">
+                    <Icon className="w-6 h-6" /> {/* SVG */}
+                    {skill} {/* Texto */}
+                    </span>
+                );
+                })}
+
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 }
