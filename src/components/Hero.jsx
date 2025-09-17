be94139fd797c9efd12ae17fb7de0175a41102d1
TheBos-s <mkpako2009@gmail.com>
2025-09-17 10:21:36 +0200

    top padding from 20px to 28px

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,19 @@
 export default function Hero() {
   return (
     <section id="hero">
-      <div className="w-full flex flex-col justify-start items-start pt-20 px-6 text-slate-950 space-y-6">
+      <div className="w-full flex flex-col justify-start items-start pt-28 px-6 text-slate-950 space-y-6">
 
         {/* Títulos y descripción */}
         <div className="space-y-2">
           <p className="text-4xl font-semibold text-slate-800">
             Web <span className="text-indigo-600">eficiente</span>. Diseño que <span className="text-indigo-600">conecta</span>.
           </p>
-          <p className="text-lg text-slate-700/80">
+          <p className="text-xl text-slate-700/80">
             Creo experiencias digitales <span className="font-semibold text-indigo-600">únicas</span>, <span className="font-semibold text-indigo-600">intuitivas</span> y <span className="font-semibold text-indigo-600">personalizadas</span>, pensadas para cautivar a tus usuarios y reflejar tu identidad.
           </p>
         </div>
 
-        {/* Badge "Disponible para trabajar" */}
-        <div className="bg-black rounded-lg p-2 self-start">
-          <span className="text-sm font-semibold text-white">Disponible para trabajar</span>
-        </div>
-
+    
         {/* Imagen con overlay y texto encima */}
         <div className="relative mt-6 w-full max-w-md">
           <img
