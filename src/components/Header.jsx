e89160acdba3d7aabe4fda73b977ee076f504e4a
TheBos-s <mkpako2009@gmail.com>
2025-09-17 10:08:59 +0200

    Adjusted navbar responsive layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,32 @@
 export default function Header() {
   return (
     <header id="home">
-      <div className="fixed top-2 left-1/2 transform -translate-x-1/2 z-50 bg-slate-100/30 rounded-lg shadow-md backdrop-blur-sm flex items-center w-auto max-w-full text-slate-950"
-      >
+     <div className="fixed top-7 left-1/2 transform -translate-x-1/2 z-50
+                bg-slate-100/30 rounded-lg shadow-md backdrop-blur-sm
+                flex items-center text-slate-950 px-0 w-auto max-w-full header-container">
+
         
-        {/* Logo */}
-        <a href="#hero" className="text-xl bg-white shadow-sm font-extrabold rounded-lg p-2 flex-shrink-0">
+        <a
+          href="#hero"
+          className="text-xl bg-white shadow-sm font-extrabold rounded-lg p-2 flex-shrink-0"
+        >
           M<strong>|</strong>R
         </a>
 
-        {/* Navegación scrollable en móviles */}
-        <nav className="flex gap-3 text-base lg:text-lg lg:gap-6 ml-2 lg:ml-6 overflow-hidden"
-         style={{paddingInline: "clamp(0.5rem, 5vw, 5rem)"}}>
-          <a href="#about" className="whitespace-nowrap hover:text-slate-900 transition">Sobre mí</a>
-          <a href="#project" className="whitespace-nowrap hover:text-slate-900 transition">Proyectos</a>
-          <a href="#contact" className="whitespace-nowrap hover:text-slate-900 transition">Contacto</a>
+     
+        <nav
+          className="flex gap-4 text-base md:gap-14 lg:text-lg lg:gap-10 ml-auto overflow-hidden
+                     px-6 sm:px-4 md:px-10"
+        >
+          <a href="#about" className="whitespace-nowrap hover:text-slate-900 transition">
+            Sobre mí
+          </a>
+          <a href="#project" className="whitespace-nowrap hover:text-slate-900 transition">
+            Proyectos
+          </a>
+          <a href="#contact" className="whitespace-nowrap hover:text-slate-900 transition">
+            Contacto
+          </a>
         </nav>
       </div>
     </header>
