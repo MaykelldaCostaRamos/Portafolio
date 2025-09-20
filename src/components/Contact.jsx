6f5f48bc73dc689aef43dd46817ca3f6ad31507e
TheBos-s <mkpako2009@gmail.com>
2025-09-20 12:02:31 +0200

    add text content to Contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,23 +3,24 @@ export default function Contact() {
     <form
       action=""
       id="contact"
-      className="py-20 flex flex-col justify-center items-start text-black px-4 "
+      className="py-12 px-6 sm:px-8 md:px-12 flex text-slate-950"
     >
       <div className="m-2 text-lg">
-        <h2 className="text-4xl font-bold mb-8 px-2">Contact</h2>
+        <h2 className="text-4xl font-bold mb-8 px-2">Contacto</h2>
 
         <div className="mb-8">
-          <label htmlFor="name" className="font-medium px-2">
-            Name
+          <label htmlFor="nombre" className="font-medium px-2">
+            Nombre
           </label>
           <input
             type="text"
             id="name"
-            placeholder="Full Name"
+            placeholder="Nombre y apellidos"
             className="w-full mt-2 p-2 focus:outline-none focus:placeholder-transparent  bg-transparent appearance-none"
             required
           />
-          <div className="border-0 border-b border-gray-300/30 "></div>
+          <div className="border-0 border-b border-indigo-950/30" aria-hidden="true"></div>
+
         </div>
         <div className="mb-8">
           <label htmlFor="email" className="font-medium px-2">
@@ -28,31 +29,33 @@ export default function Contact() {
           <input
             type="email"
             id="email"
-            placeholder="Email Address"
+            placeholder="Dirección de email"
             className="w-full mt-2 p-2 focus:outline-none focus:placeholder-transparent bg-transparent appearance-none"
             required
           />
-          <div className="border-0 border-b border-gray-300/30 "></div>
+          <div className="border-0 border-b border-indigo-950/30" aria-hidden="true"></div>
+
         </div>
+
         <div className="mb-6">
           <label htmlFor="message" className="font-medium px-2">
-            Message
+            Mensaje
           </label>
           <textarea
-            id="message"
-            placeholder="Your message...."
+            id="mensaje"
+            placeholder="Escribe tu mensaje...."
             rows="4"
             className="w-full mt-2 p-2 focus:outline-none focus:placeholder-transparent bg-transparent appearance-none"
           ></textarea>
-          <div className="border-0 border-b border-gray-300/30 "></div>
+          <div className="border-0 border-b border-indigo-950/30" aria-hidden="true"></div>
         </div>
 
         <button
           type="submit"
-          className="w-full text-white  rounded-lg text-xl py-3 bg-gradient-to-r from-indigo-700 via-indigo-950 to-indigo-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:focus:ring-indigo-800 shadow-lg shadow-indigo-500/50 dark:shadow-lg dark:shadow-indigo-800/80"
+          className="w-full bg-black text-white font-black rounded-lg p-2"
     
         >
-          SEND REQUEST
+          Enviar
         </button>
       </div>
     </form>
